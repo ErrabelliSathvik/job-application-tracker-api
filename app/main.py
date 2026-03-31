755894ecda4835555f0d9c29ec99ee3c335d73a6
ErrabelliSathvik <sathvikerrabelli16@gmail.com>
2026-04-01 02:10:24 +0530

    Test minimal app for Render

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
 from fastapi import FastAPI
-from app.routers import auth, jobs
-from app.database import Base, engine
-from app.models import user, job   # 👈 IMPORTANT
 
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="Job Application Tracker API")
-
-app.include_router(auth.router)
-app.include_router(jobs.router)
+app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "API is running 🚀"}
-import os
-
-port = int(os.environ.get("PORT", 8000))
+    return {"message": "Render working 🚀"}
