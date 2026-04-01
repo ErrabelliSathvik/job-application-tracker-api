1cc151045599862e346318f142e32fbb8e01c066
ErrabelliSathvik <sathvikerrabelli16@gmail.com>
2026-04-01 11:34:38 +0530

    Debug Render environment

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import sys
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Render working 🚀"}
+    return {
+        "message": "Render working 🚀",
+        "python_version": sys.version,
+        "cwd": os.getcwd(),
+        "files": os.listdir()
+    }
