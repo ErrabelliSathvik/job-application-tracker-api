310b1733800de9751f346e92c8dc204315049128
ErrabelliSathvik <153796758+ErrabelliSathvik@users.noreply.github.com>
2026-04-01 22:39:27 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import os
+
+print("🚀 APP STARTING...")
+print("PORT ENV:", os.getenv("PORT"))
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+print("THIS FILE IS RUNNING")
+
