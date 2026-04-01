da0774bcdbba0f76f6f3b43301fcb9de9c55cd89
ErrabelliSathvik <sathvikerrabelli16@gmail.com>
2026-04-01 11:57:13 +0530

    Stable minimal app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
-import sys
-import os
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {
-        "message": "Render working 🚀",
-        "python_version": sys.version,
-        "cwd": os.getcwd(),
-        "files": os.listdir()
-    }
+    return {"status": "ok"}
