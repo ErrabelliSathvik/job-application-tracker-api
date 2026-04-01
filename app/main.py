66c1153dca12ee5c7e1f60c34af61c27b1a3450b
ErrabelliSathvik <sathvikerrabelli16@gmail.com>
2026-04-01 12:48:49 +0530

    Fix main file for Railway

--- app/main.py
+++ app/main.py
@@ -1,25 +1,7 @@
 from fastapi import FastAPI
 
-print("🚀 MAIN FILE LOADED")
-
-# Try importing routers
-try:
-    from app.routers import auth, jobs
-    print("✅ IMPORT SUCCESS: routers loaded")
-except Exception as e:
-    print("❌ IMPORT FAILED:", e)
-
-app = FastAPI(title="Job Tracker API")
-
-# Try including routers
-try:
-    app.include_router(auth.router)
-    app.include_router(jobs.router)
-    print("✅ ROUTERS ADDED")
-except Exception as e:
-    print("❌ ROUTER ERROR:", e)
-
+app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "API is running 🚀"}
+    return {"message": "Railway working 🚀"}
