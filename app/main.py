7513af1feb7c57e9cbbe71cfc943d7f98cf5b364
ErrabelliSathvik <sathvikerrabelli16@gmail.com>
2026-04-01 12:10:54 +0530

    Fix routers

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
 from app.routers import auth, jobs
-from app.database import Base, engine
 
-app = FastAPI(title="Job Tracker API")
-
-Base.metadata.create_all(bind=engine)
+app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(jobs.router)
-
-@app.get("/")
-def root():
-    return {"message": "API is running 🚀"}
